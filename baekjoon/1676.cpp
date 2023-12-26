67b695618671246a7a1a9642f8691a66842f1abd
Junyou Park <pjy008008@gmail.com>
2023-12-26 20:06:03 +0900

    2023-12-26

---
+++ baekjoon/1676.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	int count = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		if (i % 125 == 0) 
+			count += 3;
+		else if (i % 25 == 0) 
+			count += 2;
+		else if (i % 5 == 0) 
+			count += 1;
+	}
+	cout << count;
+	return 0;
+}
