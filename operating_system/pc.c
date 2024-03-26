36caaba450248485ec5055557f313bced5297b26
JunyouPark <pjy008008@gmail.com>
2024-03-26 17:30:51 +0900

    os

--- operating_system/pc.c
+++ operating_system/pc.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/types.h>
 
 #define BUFFER_SIZE 512
 
-int main() {
+int main()
+{
     FILE *fp;
     char buffer[BUFFER_SIZE];
 
     // ps -aux 명령 실행
     fp = popen("ps -aux | grep './linuxtown_class1' | grep -v 'grep' | awk '{print $2}'", "r");
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         perror("popen failed");
         return -1;
     }
 
     // 명령의 출력에서 PID 읽어오기
     printf("Found PIDs:\n");
-    while (fgets(buffer, BUFFER_SIZE, fp) != NULL) {
+    while (fgets(buffer, BUFFER_SIZE, fp) != NULL)
+    {
         // PID 출력
         printf("%s", buffer);
+        kill(atoi(buffer), SIGUSR1);
     }
 
     // 파일 포인터 닫기
--- operating_system/pc2.c
+++ operating_system/pc2.c
@@ -1,36 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <signal.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 
-#define BUFFER_SIZE 128
+#define BUFFER_SIZE 512
 
-int main() {
-    pid_t pid;
+int main()
+{
     FILE *fp;
     char buffer[BUFFER_SIZE];
+    char last_line[BUFFER_SIZE];
+    char *result;
+    int special_output_needed = 0;
 
-    // 외부 프로그램 실행하여 출력을 파일에 저장
-    system("../../linuxtown_class1 > pid_output.txt");
-
-    // 파일 열기
-    fp = fopen("pid_output.txt", "r");
-    if (fp == NULL) {
-        perror("파일 열기 실패");
+    // ps -aux 명령 실행
+    fp = popen("ps -aux | grep './linuxtown_class1' | grep -v 'grep' | awk '{print $2}'", "r");
+    if (fp == NULL)
+    {
+        perror("popen failed");
         return -1;
     }
 
-    // 파일에서 PID 읽어오기
-    while (fgets(buffer, BUFFER_SIZE, fp) != NULL) {
-        printf("%s", buffer); // 터미널에 출력
+    // 명령의 출력에서 PID 읽어오기
+    printf("Found PIDs:\n");
+    while (fgets(buffer, BUFFER_SIZE, fp) != NULL)
+    {
+        // PID 출력
+        printf("%s", buffer);
+        kill(atoi(buffer), SIGUSR1);
     }
 
-    // 파일 닫기
-    fclose(fp);
+    // 파일 포인터 닫기
+    pclose(fp);
 
-    // 임시 파일 삭제
-    remove("pid_output.txt");
+    // confession.txt 파일 열기
+    FILE *confession_file = fopen("../../confession.txt", "r");
+    if (confession_file == NULL)
+    {
+        perror("confession.txt 파일 열기 실패");
+        return -1;
+    }
+
+    // confession.txt 파일의 마지막 줄 찾기
+    while (fgets(buffer, BUFFER_SIZE, confession_file) != NULL)
+    {
+        strcpy(last_line, buffer); // 마지막 줄 저장
+    }
+
+    // 마지막 줄이 "!!!"인지 확인
+    result = strstr(last_line, "!!!");
+    if (result != NULL)
+    {
+        // "!!!"를 찾았을 때 특정 문자열 출력
+        printf("Special output needed!\n");
+        special_output_needed = 1;
+    }
+
+    // confession.txt 파일 닫기
+    fclose(confession_file);
 
     return 0;
 }
